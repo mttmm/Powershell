9845f7d183dc85b583d064425a8d0e129a25ddf9
mttmm <mmaas21@gmail.com>
2017-08-29 14:28:58 -0400

    Add files via upload

---
+++ Operational/Disable_SMBv1.ps1
@@ -0,0 +1,15 @@
+﻿Get-ItemProperty HKLM:\SYSTEM\CurrentControlSet\Services\LanmanServer\Parameters
+    #Set-ItemProperty -Path "HKLM:\SYSTEM\CurrentControlSet\Services\LanmanServer\Parameters" SMB1 -Type DWORD -Value 0 –Force
+    Remove-ItemProperty -Path "HKLM:\SYSTEM\CurrentControlSet\Services\LanmanServer\Parameters" SMB1
+Get-ItemProperty HKLM:\SYSTEM\CurrentControlSet\Services\mrxsmb10
+    #Set-ItemProperty "HKLM:\SYSTEM\CurrentControlSet\Services\mrxsmb10" -Name 'Start' -Value "4"
+       #Set-ItemProperty "HKLM:\SYSTEM\CurrentControlSet\Services\mrxsmb10" -Name 'Start' -Value "2"
+
+Get-ItemProperty  hklm:\SYSTEM\CurrentControlSet\Services\LanmanWorkstation
+    #Set-ItemProperty hklm:\SYSTEM\CurrentControlSet\Services\LanmanWorkstation -Name "DependOnService" -Value ("Browser","MRxSMB20","NSI")
+
+
+
+Get-SmbServerConfiguration | select enablesmb1protocol
+
+Set-SmbServerConfiguration -EnableSMB1Protocol $False -Force
