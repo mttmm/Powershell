ef67f7e92023965bf69647480f215694f31ab218
mttmm <mmaas21@gmail.com>
2017-09-27 15:15:02 -0400

    Add files via upload

---
+++ Operational/progressbar.ps1
@@ -0,0 +1,13 @@
+﻿$totalTimes = 10
+
+  $i = 0
+
+  for ($i=0;$i -lt $totalTimes; $i++) {
+
+  $percentComplete = ($i / $totalTimes) * 100
+
+  Write-Progress -Activity 'Doing thing' -Status "Did thing $i  times" -PercentComplete $percentComplete
+
+  sleep 1
+
+}
