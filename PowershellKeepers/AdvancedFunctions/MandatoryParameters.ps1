0aa7d18b9873de0b2be05491673eaf89bae2f2e3
mttmm <mmaas21@gmail.com>
2017-09-30 08:34:16 -0400

    Add files via upload

---
+++ PowershellKeepers/AdvancedFunctions/MandatoryParameters.ps1
@@ -0,0 +1,73 @@
+﻿function New-VirtualMachine
+{
+	[CmdletBinding()]
+	param
+	(
+		[Parameter(Mandatory)]
+		[ValidateCount(1, 5)]
+		[string[]]$Name,
+		
+		[Parameter()]
+		[ValidateNotNullOrEmpty()]
+		[ValidateRange(1, 5)]
+		[int]$Count = 1,
+		
+		[Parameter()]
+		[ValidateNotNull()]
+		[ValidateRange(512MB, 1024MB)]
+		[int]$MemoryStartupBytes,
+		
+		[Parameter()]
+		[ValidateNotNullOrEmpty()]
+		[ValidateSet('1', '2')]
+		[int]$Generation = 2,
+		
+		[Parameter()]
+		[ValidateScript({
+			if (Test-Connection -ComputerName $_ -Quiet -Count 1)
+			{
+				throw "The computer [$_] is offline. Try another"
+			}
+			else
+			{
+				$true
+			}
+		})]
+		[ValidatePattern('^C:\\')]
+		[string]$Path = 'C:\somebogusfolder',
+		
+		[Parameter()]
+		[AllowNull()]
+		[string]$OperatingSystem,
+		
+		[Parameter()]
+		[ValidateNotNullOrEmpty()]
+		[string]$NullParamTest,
+		
+		[Parameter()]
+		[AllowNull()]
+		[string]$AllowNullParam
+	)
+}
+
+New-VirtualMachine
+
+New-VirtualMachine -Name 'MYNEWVM'
+
+#region Get-Help to find Mandatory parameters in cmdlets
+
+## Using help optional parmameters and their values are shown in brackets
+
+## No mandatory params
+(Get-Help -Name Get-Service -Detailed).syntax
+
+## Only Path is mandatory
+(Get-Help -Name Get-Content -Detailed).syntax
+
+#endregion
+
+#region Get-Command to find Mandatory parameters in cmdlets
+
+(Get-Command -Name 'Get-Content').Parameters.Path.Attributes
+
+#endregion
---
+++ PowershellKeepers/AdvancedFunctions/ParameterSets.ps1
@@ -0,0 +1,79 @@
+﻿function New-VirtualMachine
+{
+	[CmdletBinding(DefaultParameterSetName = 'None')]
+	param
+	(
+		[Parameter(Mandatory, ParameterSetName = 'ByName')]
+		[ValidateCount(1, 5)]
+		[string[]]$Name,
+		
+		[Parameter(Mandatory, ParameterSetName = 'ById')]
+		[ValidateNotNullOrEmpty()]
+		[string]$Id,
+		
+		[Parameter()]
+		[ValidateNotNullOrEmpty()]
+		[ValidateRange(1, 5)]
+		[int]$Count = 1,
+		
+		[Parameter()]
+		[ValidateNotNull()]
+		[ValidateRange(512MB, 1024MB)]
+		[int]$MemoryStartupBytes,
+		
+		[Parameter()]
+		[ValidateNotNullOrEmpty()]
+		[ValidateSet('1', '2')]
+		[int]$Generation = 2,
+		
+		[Parameter()]
+		[ValidateScript({ Test-Path -Path $_ -PathType Container })]
+		[ValidatePattern('^C:\\')]
+		[string]$Path = 'C:\somebogusfolder',
+		
+		[Parameter()]
+		[AllowNull()]
+		[string]$OperatingSystem
+	)
+	
+	if ($PSCmdlet.ParameterSetName -eq 'ById')
+	{
+		Write-Host "You're using the ById parameter set by using the Id parameter [$($Id)]"
+	}
+	elseif ($PSCmdlet.ParameterSetName -eq 'ByName')
+	{
+		Write-Host "You're using the ByName parameter set by using the Name parameter [$($Name)]"	
+	}
+
+    Write-host $count
+}
+
+#region Parameter sets with mandatory parameters
+New-VirtualMachine -Name 'MYNEWVM'
+New-VirtualMachine -Id 'HYPERVHOST1'
+#endregion
+
+#region Parameter sets with optional params (Default)
+
+New-VirtualMachine ## will not work
+
+## Add a default parameter set 
+New-VirtualMachine -Count 2
+
+#endregion
+
+#region in help
+help New-VirtualMachine
+
+help Test-Connection
+#endregion
+
+#region Using Get-Command
+## Add default parameter set
+$command = Get-Command New-VirtualMachine
+$command.DefaultParameterSet
+$command.ParameterSets | Select name
+
+## Find params inside of parameter sets
+($command.ParameterSets[2]).Parameters | ft name
+#endregion
