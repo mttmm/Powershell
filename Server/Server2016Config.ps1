5d725c7f0906e1a3f23cf5bc88d31a804077419b
mttmm <mmaas21@gmail.com>
2017-10-29 16:40:40 -0400

    Add files via upload

--- Server/Server2016Config.ps1
+++ Server/Server2016Config.ps1
@@ -1,3 +1,7 @@
 ﻿# Server 2016 Services
-Get-Service -Name MapsBroker | Set-Service -StartupType Disabled
 Get-ScheduledTask -TaskName *XblGame* | Disable-ScheduledTask
+$Services = 'CDPUserSvc', 'MapsBroker', 'PcaSvc', 'ShellHWDetection', 'OneSyncSvc', 'WpnService'
+
+foreach ($Service in $Services) {
+    Stop-Service -Name $Service -PassThru -Verbose | Set-Service -StartupType Disabled -Verbose
+}
