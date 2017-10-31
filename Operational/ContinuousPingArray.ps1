2aac7c23e7dc0c15d187838241e45dcc5783b1d6
mttmm <mmaas21@gmail.com>
2017-10-31 15:03:11 -0400

    Add files via upload

---
+++ Operational/ContinuousPingArray.ps1
@@ -0,0 +1,14 @@
+﻿
+
+$pcs = "RAD7-GNL-01","RAD7-GHP-01","RAD7-GEN-01"
+while ($true){
+foreach ($pc in $pcs){
+    if ( Test-Connection -ComputerName $pc -Count 1 -ErrorAction SilentlyContinue)
+        {Write-host "$pc is up"}
+        else {
+        Get-date ; Write-Warning "$pc is down"
+        }
+
+}
+}
+
