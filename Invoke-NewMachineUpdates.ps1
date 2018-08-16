317625911fc5a1defa619d03bc08f6069cc34d7d
mttmm <mmaas21@gmail.com>
2018-08-16 10:33:51 -0400

    updated

---
+++ Invoke-NewMachineUpdates.ps1
@@ -0,0 +1,1 @@
+Install-PackageProvider -Name NuGet -MinimumVersion 2.8.5.201 -Force;Install-Module 'PSWindowsUpdate' -Repository 'PSGallery' -SkipPublisherCheck -Force ;Import-Module 'PSWindowsUpdate';Get-WUInstall -MicrosoftUpdate -Install -AcceptAll -AutoReboot
