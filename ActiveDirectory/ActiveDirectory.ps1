d8770a050f04d59e3b049edf1d36ba0b4e4548de
Matt Maas <matt.maas@radpartners.com>
2018-10-12 14:36:52 -0400

    added get-sha

--- ActiveDirectory/ActiveDirectory.ps1
+++
@@ -1,3 +0,0 @@
-﻿Get-ADDomainController -Discover:$true -DomainName:"MTTMMTECH" -ForceDiscover:$true -Service:ADWS -Writable:$true
-New-ADOrganizationalUnit -Name:"Servers" -Path:"DC=mttmmtech,DC=com" -ProtectedFromAccidentalDeletion:$true -Server:"DC01.mttmmtech.com"
-Set-ADObject -Identity:"OU=Servers,DC=mttmmtech,DC=com" -ProtectedFromAccidentalDeletion:$true -Server:"DC01.mttmmtech.com"
